f5f0d0bf163d68b892a257201766a353eb33febf
chirikatori <thanhluffy5D@gmail.com>
2024-12-13 10:05:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from data_streaming import (
     Consumer,
     ScrapeData
 )
-from pyspark.sql.types import StructType, StringType
-# import os
-# os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.3,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3 pyspark-shell' # noqa
 
 
 if __name__ == "__main__":
@@ -15,7 +12,5 @@ if __name__ == "__main__":
     producer = Producer(scraped_data=data)
     producer.send_messages()
 
-    schema = StructType() \
-        .add("comment", StringType())
-    consumer = Consumer(schema=schema)
+    consumer = Consumer()
     consumer.start_stream()
